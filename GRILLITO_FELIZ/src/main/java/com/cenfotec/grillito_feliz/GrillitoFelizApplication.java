d07b853a7b0e9cd8ba2dbc4bcc08bb5f4fa390b6
squirosh <squirosh@ucenfotec.ac.cr>
2022-08-21 23:49:12 -0600

    commit

---
+++ GRILLITO_FELIZ/src/main/java/com/cenfotec/grillito_feliz/GrillitoFelizApplication.java
@@ -0,0 +1,13 @@
+package com.cenfotec.grillito_feliz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrillitoFelizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrillitoFelizApplication.class, args);
+	}
+
+}
---
+++ GRILLITO_FELIZ/src/test/java/com/cenfotec/grillito_feliz/GrillitoFelizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenfotec.grillito_feliz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrillitoFelizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
